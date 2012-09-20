fe18534c2bd57707941bcd04f1201f712443b035
SangChan <gyaleon@kthcorp.com>
2012-09-20 10:30:48 +0900

    camera enable

--- FaceDetect/ViewController.m
+++ FaceDetect/ViewController.m
@@ -8,11 +8,14 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
-@interface ViewController ()
-@property(nonatomic,strong) AVCaptureSession *session;
-@property(nonatomic,strong) AVCaptureDevice *videoDevice;
-@property(nonatomic,strong) AVCaptureDeviceInput *videoInput;
-@property(nonatomic,strong) AVCaptureVideoDataOutput *frameOutput;
+
+@interface ViewController () <AVCaptureAudioDataOutputSampleBufferDelegate>
+@property (nonatomic, strong) AVCaptureSession *session;
+@property (nonatomic, strong) AVCaptureDevice *videoDevice;
+@property (nonatomic, strong) AVCaptureDeviceInput *videoInput;
+@property (nonatomic, strong) AVCaptureVideoDataOutput *frameOutput;
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
+@property (nonatomic, strong) CIContext *context;
 @end
 
 @implementation ViewController
@@ -21,6 +24,24 @@
 @synthesize videoDevice = _videoDevice;
 @synthesize videoInput = _videoInput;
 @synthesize frameOutput = _frameOutput;
+@synthesize imgView = _imgView;
+@synthesize context = _context;
+
+-(CIContext *)context {
+    if (!_context) {
+        _context = [CIContext contextWithOptions:nil];
+    }
+    return _context;
+}
+
+-(void) captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    CVImageBufferRef pb = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CIImage *ciImage  = [CIImage imageWithCVPixelBuffer:pb];
+    
+    CGImageRef ref = [self.context createCGImage:ciImage fromRect:ciImage.extent];
+    self.imgView.image = [UIImage imageWithCGImage:ref scale:1.0 orientation:UIImageOrientationRight];
+    CGImageRelease(ref);
+}
 
 - (void)viewDidLoad
 {
@@ -32,6 +53,15 @@
     self.videoDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     self.videoInput = [AVCaptureDeviceInput deviceInputWithDevice:self.videoDevice error:nil];
     
+    self.frameOutput = [[AVCaptureVideoDataOutput alloc]init];
+    self.frameOutput.videoSettings = [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+    
+    [self.session addInput:self.videoInput];
+    [self.session addOutput:self.frameOutput];
+    
+    [self.frameOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
+    
+    [self.session startRunning];
     
 }
 
