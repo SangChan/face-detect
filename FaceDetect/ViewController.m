39247c090c8fbf2aca7497d947634b7ee42df603
SangChan <gyaleon@kthcorp.com>
2012-09-24 15:19:56 +0900

    add test code..

--- FaceDetect/ViewController.m
+++ FaceDetect/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CoreGraphics/CoreGraphics.h>
 #import <AVFoundation/AVFoundation.h>
 
 @interface ViewController () <AVCaptureAudioDataOutputSampleBufferDelegate>
@@ -17,6 +18,10 @@
 @property (nonatomic, strong) CIContext *context;
 @property (nonatomic, strong) CIDetector *faceDetector;
 @property (nonatomic, strong) UIImageView * glasses;
+@property (nonatomic, strong) UIView * leftEyeView;
+@property (nonatomic, strong) UIView * rightEyeView;
+@property (nonatomic, strong) UIView * mouthView;
+@property (nonatomic, strong) UIView * faceView;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
 @property (strong, nonatomic) IBOutlet UIView *preview;
 @end
@@ -32,6 +37,9 @@
 @synthesize glasses = _glasses;
 @synthesize preview = _preview;
 @synthesize previewLayer = _previewLayer;
+@synthesize leftEyeView = _leftEyeView;
+@synthesize rightEyeView = _rightEyeView;
+@synthesize mouthView = _mouthView;
 
 -(CIContext *)context {
     if (!_context) {
@@ -40,14 +48,22 @@
     return _context;
 }
 
+#define degreesToRadians(degrees) ((degrees)/180.0 * M_PI)
+
 -(void) captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
     CVPixelBufferRef pb = CMSampleBufferGetImageBuffer(sampleBuffer);
     CFDictionaryRef attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate);
     CIImage *ciImage  = [[CIImage alloc]initWithCVPixelBuffer:pb options:(__bridge NSDictionary*)attachments];
-    
     if (attachments) {
         CFRelease(attachments);
     }
+    
+//    CIFilter *scaleFilter = [CIFilter filterWithName:@"CILanczosScaleTransform"];
+//    [scaleFilter setValue:ciImage forKey:@"inputImage"];
+//    [scaleFilter setValue:[NSNumber numberWithFloat:1.0] forKey:@"inputScale"];
+//    [scaleFilter setValue:[NSNumber numberWithFloat:1.0] forKey:@"inputAspectRatio"];
+//    CIImage *finalImage = [scaleFilter valueForKey:@"outputImage"];
+    
     
     NSArray *features = [self.faceDetector featuresInImage:ciImage
                                                    options:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:6]
@@ -55,7 +71,64 @@
     
     BOOL faceFound = NO;
     for (CIFaceFeature *face in features) {
-        NSLog(@"%@",[face description]);
+        //NSLog(@"%@",[face description]);
+        
+        CGRect faceRect = [face bounds];
+        CGFloat temp = faceRect.size.width;
+		faceRect.size.width = faceRect.size.height;
+		faceRect.size.height = temp;
+		temp = faceRect.origin.x;
+		faceRect.origin.x = faceRect.origin.y;
+		faceRect.origin.y = temp;
+        
+        CGFloat widthScaleBy = self.preview.bounds.size.width / ciImage.extent.size.height;
+		CGFloat heightScaleBy = self.preview.bounds.size.height / ciImage.extent.size.width;
+        
+        faceRect.size.width *= widthScaleBy;
+		faceRect.size.height *= heightScaleBy;
+		faceRect.origin.x *= widthScaleBy;
+		faceRect.origin.y *= heightScaleBy;
+        
+        
+        NSLog(@"face x = %f, y = %f, w = %f , h = %f",face.bounds.origin.x,face.bounds.origin.y
+              ,face.bounds.size.width,face.bounds.size.height);
+        
+        
+        [self.faceView setFrame:faceRect];
+        [self.faceView setHidden:NO];
+//        if (face.hasLeftEyePosition) {
+//            NSLog(@"leftEye x = %f, y = %f",face.leftEyePosition.x,face.leftEyePosition.y);
+//            CGRect leftEye = CGRectMake(face.leftEyePosition.y, face.leftEyePosition.x
+//                                        , 4, 4);
+//            [self.leftEyeView setFrame:leftEye];
+//            //[self.leftEyeView setTransform:CGAffineTransformMakeRotation(degreesToRadians(180))];
+//            [self.leftEyeView setHidden:!face.hasLeftEyePosition];
+//            
+//        }
+//        
+//        if (face.hasRightEyePosition) {
+//            NSLog(@"rightEye x = %f, y = %f",face.rightEyePosition.x,face.rightEyePosition.y);
+//            CGRect rightEye = CGRectMake(face.rightEyePosition.y, face.rightEyePosition.x
+//                                        , 4, 4);
+//            [self.rightEyeView setFrame:rightEye];
+//            //[self.rightEyeView setTransform:CGAffineTransformMakeRotation(degreesToRadians(180))];
+//            [self.rightEyeView setHidden:!face.hasRightEyePosition];
+//            
+//        }
+//        
+//        if (face.hasMouthPosition) {
+//            NSLog(@"Mouth x = %f, y = %f",face.mouthPosition.x,face.mouthPosition.y);
+//            CGRect mouth = CGRectMake(face.mouthPosition.y, face.mouthPosition.x
+//                                        , 4, 4);
+//            [self.mouthView setFrame:mouth];
+//            //[self.mouthView setTransform:CGAffineTransformMakeRotation(degreesToRadians(180))];
+//            [self.mouthView setHidden:!face.hasMouthPosition];
+//            
+//        }
+        
+        if (face.hasTrackingID) {
+            NSLog(@"tracking ID = %d",face.trackingID);
+        }
         if (face.hasLeftEyePosition && face.hasRightEyePosition) {
             CGPoint eyeCenter = CGPointMake(face.leftEyePosition.x*0.5+face.rightEyePosition.x*0.5, face.leftEyePosition.y*0.5+face.rightEyePosition.y*0.5);
                 
@@ -81,8 +154,8 @@
         }
     }
     
-    [self.glasses setHidden:!faceFound];
-    NSLog(@"glasses = %@",[self.glasses description]);
+    //[self.glasses setHidden:!faceFound];
+   // NSLog(@"glasses = %@",[self.glasses description]);
 }
 
 - (void)viewDidLoad
@@ -132,6 +205,32 @@
     self.glasses = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"glasses.png"]];
     [self.glasses setHidden:YES];
     [self.preview addSubview:self.glasses];
+    
+    self.faceView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 10)];
+    [self.faceView setBackgroundColor:[UIColor blackColor]];
+    [self.faceView setAlpha:0.2];
+    [self.faceView setOpaque:YES];
+    [self.faceView setHidden:YES];
+    [self.preview addSubview:self.faceView];
+    
+    self.leftEyeView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 10)];
+    [self.leftEyeView setBackgroundColor:[UIColor blueColor]];
+    [self.leftEyeView setOpaque:YES];
+    [self.leftEyeView setHidden:YES];
+    [self.preview addSubview:self.leftEyeView];
+    
+    self.rightEyeView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 10)];
+    [self.rightEyeView setBackgroundColor:[UIColor redColor]];
+    [self.rightEyeView setOpaque:YES];
+    [self.rightEyeView setHidden:YES];
+    [self.preview addSubview:self.rightEyeView];
+    
+    self.mouthView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 10)];
+    [self.mouthView setBackgroundColor:[UIColor brownColor]];
+    [self.mouthView setOpaque:YES];
+    [self.mouthView setHidden:YES];
+    [self.preview addSubview:self.mouthView];
+    
 }
 
 - (void)didReceiveMemoryWarning
